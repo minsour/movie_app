dec0b167447918f3a8c901af39dd5fd15eff26df
DevSoopark <sparkm0128@gmail.com>
2018-12-29 10:51:23 +0900

    Updates

---
+++ precache-manifest.d20c44472fb143d0c6268525d89c5bcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "1d387b975e7b685e37fb",
+    "url": "/movie_app/static/js/main.1d387b97.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "1d387b975e7b685e37fb",
+    "url": "/movie_app/static/css/main.17a0ec88.chunk.css"
+  },
+  {
+    "revision": "e3ba2d4084a58185f9c386070b295ed7",
+    "url": "/movie_app/index.html"
+  }
+];
